0947093375d51e356649fa387268acead19e9145
Mayank Sharma <mayanksharma1893@gmail.com>
2019-06-20 17:11:47 -0500

    test

--- application.py
+++ application.py
@@ -9,53 +9,56 @@ app = Flask(__name__)
 port = int(os.getenv("PORT", 5000))
 
 @app.route('/')
-def index():
+def hello_world():
+    con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    a=10
+    start=0
+    end=100
+    age_interval=[0]
+    val=start
+    while val<end:
+        val+=10
+        age_interval.append(val)
+    # columns=['age_group','count']  
+    # print(age_interval)
+    mem=[]
+    # memdict=dict()
+    # for i in range(0,len(age_interval)-1):
+    #     query="select StateName,(Voted/TotalPop*100) AS Perc from voting"
+    #     cur=con.cursor()
+    #     cur.execute(query)
+    #     result=list(cur.fetchall())
+        
+    #     age_group=str(age_interval[i])+"-"+str(age_interval[i+1])
+    #     for row in result: 
+    #         memdict=dict()
+    #         for j,val in enumerate(row):
+    #             memdict["StateName"]=StateName  
+    #             memdict["Perc"]=Perc
+    #             mem.append(memdict)
+    #         print(mem)
 
-    return render_template('index.html')
+    #     # print(mem)
+    # return render_template('chart.html',a=mem,chart="pie")
 
 
-
-@app.route('/Search5', methods=['GET', 'POST'])
-def Search5():
-
-    cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    cursor = cnxn.cursor()
-    cursor.execute("Select StateName,TotalPop from voting where TotalPop between '5000' and '10000'")
-    row = cursor.fetchall()
-    return render_template("view.html", row=row)
-
-
-
-@app.route('/Search10', methods=['GET', 'POST'])
-def Search10():
     
-    cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    cursor = cnxn.cursor()
-    cursor.execute("Select StateName,TotalPop from voting where TotalPop between '10000' and '50000'")
-    row = cursor.fetchall()
-    return render_template("view.html", row=row)
-
-
-@app.route('/chart',methods=['GET', 'POST'])
-def chart():
-        mag = request.form['mag1']
-        con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-        query="Select mag,latitude from quake3 where mag >= 7"
-        columns=['mag','latitude']
-        dic=dict()
-        cur=con.cursor()
-        mem=[]
-        cur.execute(query)
-        result=list(cur.fetchall())
-        for row in result:
-            memdict=dict()
-            for j,val in enumerate(row):
-                memdict[columns[j]]=val
-            mem.append(memdict)
-        # print(mem)
-        a=[1,2,3,4,5]
-        # print(a)
-        return render_template('chart.html',a=mem,chart="bar")
+    query="select StateName,(Voted/TotalPop*100) AS Perc from voting"
+    columns=['StateName','Perc']
+    dic=dict()
+    cur=con.cursor()
+    mem=[]
+    cur.execute(query)
+    result=list(cur.fetchall())
+    for row in result:
+        memdict=dict()
+        for j,val in enumerate(row):
+            memdict[columns[j]]=val
+        mem.append(memdict)
+    # print(mem)
+    a=[1,2,3,4,5]
+    # print(a)
+    return render_template('chart.html',a=mem,chart="pie")
 
 # @app.route('/streaming.csv')
 # def streaming():
@@ -78,10 +81,97 @@ def chart():
 #     return tuple(result)
 
 
-port = os.getenv("PORT", 5000)
 
 if __name__ == '__main__':
-   app.run(debug="true",port=int(port))
+    app.run(port=port,debug=True)
+
+
+
+
+# """Cloud Foundry test"""
+# from flask import Flask,render_template
+# import os
+# import pyodbc
+# import csv
+
+# app = Flask(__name__)
+
+# port = int(os.getenv("PORT", 5000))
+
+# @app.route('/')
+# def index():
+
+#     return render_template('index.html')
+
+
+
+# @app.route('/Search5', methods=['GET', 'POST'])
+# def Search5():
+
+#     cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+#     cursor = cnxn.cursor()
+#     cursor.execute("Select StateName,TotalPop from voting where TotalPop between '5000' and '10000'")
+#     row = cursor.fetchall()
+#     return render_template("view.html", row=row)
+
+
+
+# @app.route('/Search10', methods=['GET', 'POST'])
+# def Search10():
+    
+#     cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+#     cursor = cnxn.cursor()
+#     cursor.execute("Select StateName,TotalPop from voting where TotalPop between '10000' and '50000'")
+#     row = cursor.fetchall()
+#     return render_template("view.html", row=row)
+
+
+# @app.route('/chart',methods=['GET', 'POST'])
+# def chart():
+#         mag = request.form['mag1']
+#         con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:mayankazuredb.database.windows.net,1433;Database=azuredbtest;Uid=mayanksharma1893@mayankazuredb;Pwd={Mayank180493#};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+#         query="Select mag,latitude from quake3 where mag >= 7"
+#         columns=['mag','latitude']
+#         dic=dict()
+#         cur=con.cursor()
+#         mem=[]
+#         cur.execute(query)
+#         result=list(cur.fetchall())
+#         for row in result:
+#             memdict=dict()
+#             for j,val in enumerate(row):
+#                 memdict[columns[j]]=val
+#             mem.append(memdict)
+#         # print(mem)
+#         a=[1,2,3,4,5]
+#         # print(a)
+#         return render_template('chart.html',a=mem,chart="bar")
+
+# # @app.route('/streaming.csv')
+# # def streaming():
+# #     result=[]
+# #     with open('streaming.csv') as csv_file:
+        
+# #         csv_reader = csv.reader(csv_file, delimiter=',')
+        
+# #         line_count = 0
+# #         for row in csv_reader:
+# #             result.append(row)
+# #             if line_count == 0:
+# #                 print(f'Column names are {", ".join(row)}')
+# #                 line_count += 1
+# #             else:
+# #                 print(row)
+# #                 line_count += 1
+# #             print(f'Processed {line_count} lines.')
+        
+# #     return tuple(result)
+
+
+# port = os.getenv("PORT", 5000)
+
+# if __name__ == '__main__':
+#    app.run(debug="true",port=int(port))
 
 
 
